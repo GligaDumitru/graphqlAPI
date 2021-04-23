e00247868d3d38452c128dba3148a8845fcb8a1f
GligaDumitru <gliga_dumitru@yahoo.com>
2021-04-23 22:54:05 +0300

    asda

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const Post = require('./models/Post');
 const { MONGODB } = require('./config');
 
 const server = new ApolloServer({
@@ -14,13 +12,8 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
-// Connect to DB
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to DB');
-    return server.listen({ port: 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
+    return server.listen({ port: process.env.PORT || 5000 });
   });
